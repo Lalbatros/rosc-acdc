3e8da5ab4e97de12f574afe4f2d74169cc8084fc
Dileep Deesh <deesh.dileep@eliagrid-int.com>
2026-09-15 10:11:29 +0200

    Update to conside contingencies from CO files

--- rosc_acdc/contingencies.py
+++ rosc_acdc/contingencies.py
@@ -45,6 +45,36 @@ def load_contingency_data():
         logger.info("RCC-only contingencies: %d", len(set(con_ids_rcc) - set(con_ids_ncc)))
         logger.info("Combined contingencies: %d", len(con_ids_all))
         return data_all
+    elif config.CON_OPT==3:
+
+        import pandas as pd
+        import xml.etree.ElementTree as ET
+
+        def parse_co(path):
+            ns = {'cim':'http://iec.ch/TC57/CIM100#','nc':'http://entsoe.eu/ns/nc#'}
+            RES = '{http://www.w3.org/1999/02/22-rdf-syntax-ns#}resource'
+            root = ET.parse(path).getroot()
+
+            eq_rows = []
+            for el in root.findall('cim:ContingencyEquipment', ns):
+                eq_rows.append({
+                    'mRID': el.findtext('cim:IdentifiedObject.mRID', namespaces=ns),
+                    'name': el.findtext('cim:IdentifiedObject.name', namespaces=ns),
+                    'Contingency': el.find('cim:ContingencyElement.Contingency', ns).get(RES).lstrip('#').lstrip('_'),
+                    'Equipment': el.find('cim:ContingencyEquipment.Equipment', ns).get(RES).lstrip('#').lstrip('_'),
+                })
+
+            co_rows = []
+            for el in root.findall('nc:OrdinaryContingency', ns):
+                co_rows.append({
+                    'mRID': el.findtext('cim:IdentifiedObject.mRID', namespaces=ns),
+                    'name': el.findtext('cim:IdentifiedObject.name', namespaces=ns),
+                })
+
+            return pd.DataFrame(eq_rows), pd.DataFrame(co_rows)
+
+        df_equipment, df_contingency = parse_co(config.CO_XML_loc)
+        return [df_equipment, df_contingency ]
 
     # CON_OPT == 2: use only RCC contingencies
     with open(config.json_file_rcc, "r") as file_temp:
@@ -88,40 +118,65 @@ def add_contingencies_and_actions(security_analysis, data, valid_ids):
 
     missing = []
     k_con = 0
-    for scenario_name, case in data.items():
-        elements_ids = []
-        for comp in case.get("InterruptedComponents", []) + case.get("OpenedSwitches", []):
-            rdf_id = normalize(comp["RdfId"])
-            if rdf_id in valid_ids:
-                elements_ids.append(rdf_id)
-            elif rdf_id:
-                missing.append((scenario_name, comp["Name"], rdf_id))
+    if config.CON_OPT!=3:
+        for scenario_name, case in data.items():
+            elements_ids = []
+            for comp in case.get("InterruptedComponents", []) + case.get("OpenedSwitches", []):
+                rdf_id = normalize(comp["RdfId"])
+                if rdf_id in valid_ids:
+                    elements_ids.append(rdf_id)
+                elif rdf_id:
+                    missing.append((scenario_name, comp["Name"], rdf_id))
 
-        if elements_ids:
-            k_con += 1
-            security_analysis.add_multiple_elements_contingency(
-                elements_ids,
-                contingency_id=scenario_name,
-            )
+            if elements_ids:
+                k_con += 1
+                security_analysis.add_multiple_elements_contingency(
+                    elements_ids,
+                    contingency_id=scenario_name,
+                )
 
-        actions = []
-        for sw in case.get("ClosedSwitches", []):
-            sw_rdfid = normalize(sw["RdfId"])
-            if sw_rdfid in valid_ids:
-                action_id = f"{scenario_name}_close_{sw_rdfid}"
-                security_analysis.add_switch_action(action_id, switch_id=sw_rdfid, open=False)
-                actions.append(action_id)
-            else:
-                missing.append((scenario_name, sw["Name"], sw_rdfid))
+            actions = []
+            for sw in case.get("ClosedSwitches", []):
+                sw_rdfid = normalize(sw["RdfId"])
+                if sw_rdfid in valid_ids:
+                    action_id = f"{scenario_name}_close_{sw_rdfid}"
+                    security_analysis.add_switch_action(action_id, switch_id=sw_rdfid, open=False)
+                    actions.append(action_id)
+                else:
+                    missing.append((scenario_name, sw["Name"], sw_rdfid))
 
-        if elements_ids and actions:
-            security_analysis.add_operator_strategy(
-                f"{scenario_name}_strategy",
-                contingency_id=scenario_name,
-                action_ids=actions,
-                condition_type=pp.security.ConditionType.TRUE_CONDITION,
-            )
+            if elements_ids and actions:
+                security_analysis.add_operator_strategy(
+                    f"{scenario_name}_strategy",
+                    contingency_id=scenario_name,
+                    action_ids=actions,
+                    condition_type=pp.security.ConditionType.TRUE_CONDITION,
+                )
+    else: #USING CO LIST FROM XML
+        [df_equipment, df_contingency ] = data
+        df_contingency=df_contingency.set_index("mRID")
+        list_con=df_equipment["Contingency"].drop_duplicates()
+        for case_con in list_con:
+            try:
+                scenario_name=df_contingency.loc[case_con].values[0]
+            except:
+                scenario_name=case_con
+                print("CANNOT FIND NAME for",case_con)
 
+            list_elements=df_equipment[df_equipment["Contingency"]==case_con]
+            elements_ids = []
+            for temploc in range(len(list_elements)):
+                if list_elements["mRID"].iloc[temploc] in valid_ids:
+                    elements_ids.append(list_elements["mRID"].iloc[temploc])
+                else:
+                    missing.append(list_elements["mRID"].iloc[temploc])
+
+            if elements_ids:
+                k_con+=1
+                security_analysis.add_multiple_elements_contingency(
+                    elements_ids,
+                    contingency_id=scenario_name
+                )
     if missing:
         logger.warning("Skipped %d missing elements in contingencies", len(missing))
 
